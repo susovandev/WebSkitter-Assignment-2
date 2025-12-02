14ea705e4c0e25dd00c6f73250ab5cba721d7ecd
Susovan Dev <susovandas.dev@gmail.com>
2025-12-02 08:59:21 +0530

    feat(database): add in-memory dummy database for product CRUD operations

---
+++ app/db/db.js
@@ -0,0 +1,28 @@
+const productDatabase = [
+	{
+		_id: 1,
+		name: 'Product 1',
+		description: 'Product 1 description',
+		price: 100,
+		category: 'Men',
+		inStock: true,
+	},
+	{
+		_id: 2,
+		name: 'Product 2',
+		description: 'Product 2 description',
+		price: 200,
+		category: 'Female',
+		inStock: false,
+	},
+	{
+		_id: 3,
+		name: 'Product 3',
+		description: 'Product 3 description',
+		price: 300,
+		category: 'Men',
+		inStock: true,
+	},
+];
+
+module.exports = productDatabase;
